cc6783c566c977a21b651bc980b139428410ce4d
ssuude <sdnr.elmas@hotmail.com>
2024-01-02 20:42:26 +0300

    roles eklendi

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "blogg"
+    },
+    email: {
+        username: "<email>",
+        password: "<parola>",
+        from: "<from_email>",
+    }
+}
+
+module.exports = config;
